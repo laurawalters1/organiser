c3015033b1575d85757aeed3205d45425cb822c4
Laura Walters <laura.walters1098@yahoo.com>
2022-10-30 14:50:10 +0000

    user signup backend

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,23 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
 const { isConstValueNode } = require("graphql");
+const { User } = require("../models");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       return "hello";
+    },
+  },
+  Mutation: {
+    //////////////////////////////////////
+    //////////////SIGNUP///////////////////
+    //////////////////////////////////////
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      // generate token
+      const token = signToken(user);
+      return { token, user };
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,24 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  type User {
+    username: String!
+    email: String!
+    password: String!
+    _id: ID!
+  }
+
+  type UserAuth {
+    token: ID!
+    user: User
+  }
+
   type Query {
     me: String
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): UserAuth
   }
 `;
 
