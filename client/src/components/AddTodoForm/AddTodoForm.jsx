4d51b88278349b5f09922789fb60ba6bf11ebff4
Laura Walters <laura.walters1098@yahoo.com>
2022-11-11 07:41:36 +0000

    added rich text editor

--- client/src/components/AddTodoForm/AddTodoForm.jsx
+++ client/src/components/AddTodoForm/AddTodoForm.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { RichTextEditor } from '../../components'
+
 
 function AddTodoForm() {
+   
+
+
+
   return (
     <form className='bg-light d-flex flex-column col-lg-4 col-8  align-items-stretch p-5 mt-5'>
          <h2 className='mb-3'>New Todo</h2>
@@ -11,7 +17,8 @@ function AddTodoForm() {
         </div>
         {/* <input type="password" placeholder='password' name='password' onChange={handleUserInput} className='my-2'/> */}
 
-       <textarea name="" id="desc" cols="30" rows="10"></textarea>
+       {/* <textarea name="" id="desc" cols="30" rows="10"></textarea> */}
+       <RichTextEditor></RichTextEditor>
        <div className='d-flex justify-content-center'>
         <button className='my-4 custom-btn-outline-brand col-5 '>Add</button>
         </div>
