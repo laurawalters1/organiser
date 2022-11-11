6883d2324c731fb60b65cacf991ed73a5a21c34b
Laura Walters <laura.walters1098@yahoo.com>
2022-11-11 07:10:08 +0000

    add todo form added

---
+++ client/src/components/AddTodoForm/AddTodoForm.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function AddTodoForm() {
+  return (
+    <form className='bg-light d-flex flex-column col-lg-4 col-8  align-items-stretch p-5 mt-5'>
+         <h2 className='mb-3'>New Todo</h2>
+         <div class=" mb-3">
+            
+        <input type="text" className="form-control col-12" id="floatingInputEmail" name='title' placeholder="Title"/>
+ 
+        </div>
+        {/* <input type="password" placeholder='password' name='password' onChange={handleUserInput} className='my-2'/> */}
+
+       <textarea name="" id="desc" cols="30" rows="10"></textarea>
+       <div className='d-flex justify-content-center'>
+        <button className='my-4 custom-btn-outline-brand col-5 '>Add</button>
+        </div>
+    </form>
+  )
+}
+
+export default AddTodoForm
--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import AddTodoForm from '../../components/AddTodoForm/AddTodoForm'
 
 function Dashboard() {
   return (
-    <div className='text-light'>Dashboard</div>
+      <div className='d-flex flex-column align-items-center'>
+    <AddTodoForm></AddTodoForm>
+    </div>
   )
 }
 
