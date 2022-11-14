1d9145fe9e98a0a0337cd0d6661b06ccd4a3aa48
Laura Walters <laura.walters1098@yahoo.com>
2022-11-14 17:14:55 +0000

    added login and signup buttons to home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import Button from 'react-bootstrap/Button';
 function Home() {
   return (
-    <div className='text-light'>Home</div>
+    <div className='text-light d-flex justify-content-center'>
+
+<Button href='/signup'  variant="primary" className="custom-btn-outline-brand-invert mx-2 my-4">Signup</Button> 
+<Button href="/login"  variant="primary" className="custom-btn-outline-brand-invert mx-2 my-4">Login</Button> 
+
+    </div>
   )
 }
 
